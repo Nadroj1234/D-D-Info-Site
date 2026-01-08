ed6d7797a8242788d224fa24a118b83970785a78
Dakota Kuhn <dakotakuhn27@gmail.com>
2026-01-08 14:25:26 -0500

    Refactor spell fetching logic in js.js and enhance spells.html structure with additional spell information buttons

--- js.js
+++ js.js
@@ -7,7 +7,32 @@ const requestOptions = {
   redirect: "follow",
 };
 
-let spells = fetch("https://www.dnd5eapi.co/api/2014/spells/", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
+const spell = document.getElementById("spell");
+
+const spell_info = document.getElementById("spell-info");
+
+const submit_button = document.getElementById("submit-button");
+
+const spell_name_button = document.getElementById("spell-name-button");
+
+const spell_desc_button = document.getElementById("spell-desc-button");
+
+const spell_level_button = document.getElementById("spell-level-button");
+
+const spell_range_button = document.getElementById("spell-range-button");
+
+const spell_duration_button = document.getElementById("spell-duration-button");
+
+const school_button = document.getElementById("school-button");
+
+const casting_time_button = document.getElementById("casting-time-button");
+
+const classes_button = document.getElementById("classes-button");
+
+function spells() {
+  fetch("https://www.dnd5eapi.co/api/2014/spells/", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result));
+}
+
+spells();
